c42113688a28999a20bcf40c2d5ac07c6ad3bee2
maksim <maksim@DESKTOP-9D1MEP9>
2025-06-25 00:13:24 +0300

    add new migrations

---
+++ GameSite/Data/Migrations/20250624211211_StatusRoleAndAutgGoogle.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameSite.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StatusRoleAndAutgGoogle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MediaUrl",
+                table: "Posts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailPublic",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_PostId",
+                table: "Likes",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Posts_PostId",
+                table: "Likes",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Posts_PostId",
+                table: "Likes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Likes_PostId",
+                table: "Likes");
+
+            migrationBuilder.DropColumn(
+                name: "MediaUrl",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailPublic",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000);
+        }
+    }
+}

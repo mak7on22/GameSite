d440f4691c93db8cac035080ae80adc25093f506
maks <127958880+mak7on22@users.noreply.github.com>
2025-06-24 18:04:47 +0300

    Add Google authentication and custom password rules

--- GameSite/Controllers/AccountController.cs
+++ GameSite/Controllers/AccountController.cs
@@ -1,16 +1,25 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
 using GameSite.Models;
+using GameSite.Services;
 
 namespace GameSite.Controllers
 {
     public class AccountController : Controller
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IGoogleAuthService _googleAuthService;
 
-        public AccountController(SignInManager<ApplicationUser> signInManager)
+        public AccountController(SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            IGoogleAuthService googleAuthService)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
+            _googleAuthService = googleAuthService;
         }
 
         public IActionResult Register()
@@ -37,6 +46,43 @@ namespace GameSite.Controllers
         public IActionResult ChangePassword()
         {
             return RedirectToPage("/Account/Manage/ChangePassword", new { area = "Identity" });
+        }
+
+        public IActionResult GoogleLogin(string returnUrl = "/")
+        {
+            var redirectUrl = Url.Action(nameof(GoogleResponse), "Account", new { ReturnUrl = returnUrl });
+            var properties = _googleAuthService.ConfigureExternalAuthenticationProperties(redirectUrl!);
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        public async Task<IActionResult> GoogleResponse(string returnUrl = "/")
+        {
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+
+            var signIn = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false);
+            if (signIn.Succeeded)
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            var email = info.Principal.FindFirstValue(System.Security.Claims.ClaimTypes.Email);
+            if (email != null)
+            {
+                var user = new ApplicationUser { UserName = email, Email = email, RegistrationDate = DateTime.UtcNow };
+                var result = await _userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddLoginAsync(user, info);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+            }
+
+            return RedirectToAction(nameof(Login));
         }
     }
 }

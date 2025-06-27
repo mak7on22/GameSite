4397d1dc164f9516936c446b84afeda0c1fff52e
maksim <maksim@DESKTOP-9D1MEP9>
2025-06-28 00:55:12 +0300

    фикс ошибки сигнала

--- GameSite/ReactConfig.cs
+++ GameSite/ReactConfig.cs
@@ -1,17 +1,17 @@
-using React;
-using WebActivatorEx;
+//using React;
+//using WebActivatorEx;
 
-[assembly: PreApplicationStartMethod(typeof(GameSite.ReactConfig), nameof(GameSite.ReactConfig.Configure))]
+//[assembly: PreApplicationStartMethod(typeof(GameSite.ReactConfig), nameof(GameSite.ReactConfig.Configure))]
 
-namespace GameSite
-{
-    public static class ReactConfig
-    {
-        public static void Configure()
-        {
-            ReactSiteConfiguration.Configuration
-                .SetLoadBabel(true)
-                .AddScript("~/js/DurackOnline.js");
-        }
-    }
-}
+//namespace GameSite
+//{
+//    public static class ReactConfig
+//    {
+//        public static void Configure()
+//        {
+//            ReactSiteConfiguration.Configuration
+//                .SetLoadBabel(true)
+//                .AddScript("~/js/DurackOnline.js");
+//        }
+//    }
+//}
